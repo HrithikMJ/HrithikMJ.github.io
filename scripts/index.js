8b1fb025dca1cb5b95b67dff32c95aa6f8d58c38
Hrithik <hrithikmj003@gmail.com>
2021-06-24 18:56:37 +0530

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,49 @@
-	function myFunction() {
-  	document.getElementById("head-soc").classList.toggle("show");
-   }
-   document.getElementsByClassName("fa-globe").addEventListener("click",fasd);
-   function fasd(){
-   		document.getElementById("head-soc").classList.toggle("show");
+function myFunction() {
+  document.getElementById("head-soc").classList.toggle("show");
+}
+document.getElementById("globe").addEventListener("click", fasd);
+function fasd() {
+  document.getElementById("head-soc").classList.toggle("show");
 
-   }
-   window.onclick = function(event) {
-  	if (!event.target.matches('.dropbtn')) {
-    	var dropdowns = document.getElementsByClassName("content");
-    	var i;
-    	for (i = 0; i < dropdowns.length; i++) {
-      		var openDropdown = dropdowns[i];
-      		if (openDropdown.classList.contains('show')) {
-        	openDropdown.classList.remove('show');
+}
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-  }  
+  }
 }
-function res(a){
-  if (a =='Instagram'){
-    document.getElementById("heading").innerHTML=""; 
-    document.getElementById("para").innerHTML="Check <br> out my  <br> instagram!!";
+function res(a) {
+  if (a == 'Instagram') {
+    document.getElementById("heading").innerHTML = "";
+    document.getElementById("para").innerHTML = "Check <br> out my  <br> instagram!!";
   }
-  else if (a== 'GitHub') {
-    document.getElementById("heading").innerHTML=""; 
-    document.getElementById("para").innerHTML="Check <br>out my  <br> Github!!";
+  else if (a == 'GitHub') {
+    document.getElementById("heading").innerHTML = "";
+    document.getElementById("para").innerHTML = "Check <br>out my  <br> Github!!";
   }
-  else if (a=='Twitter'){
-    document.getElementById("heading").innerHTML=""; 
-    document.getElementById("para").innerHTML="Tweet <br> me!!";
+  else if (a == 'Twitter') {
+    document.getElementById("heading").innerHTML = "";
+    document.getElementById("para").innerHTML = "Tweet <br> me!!";
 
   }
-  else if (a=='email'){
-    document.getElementById("heading").innerHTML=""; 
-    document.getElementById("para").innerHTML="E-mail <br>me !!";
+  else if (a == 'email') {
+    document.getElementById("heading").innerHTML = "";
+    document.getElementById("para").innerHTML = "E-mail <br>me !!";
 
-   }
-  else{
-    document.getElementById("heading").innerHTML="social <br> links!!"; 
-    document.getElementById("para").innerHTML="";
+  }
+  else {
+    document.getElementById("heading").innerHTML = "social <br> links!!";
+    document.getElementById("para").innerHTML = "";
 
-  } 
- }  
-function out(){
-  document.getElementById("para").innerHTML=""; 
-  document.getElementById("heading").innerHTML="Hrithik MJ <br>here!!";
+  }
+}
+function out() {
+  document.getElementById("para").innerHTML = "";
+  document.getElementById("heading").innerHTML = "Hrithik MJ <br>here!!";
 }
